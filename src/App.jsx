d6e00387740c66986b508342d14a566b256f224f
juanramirezwebdev <juanluisra11@gmail.com>
2023-09-16 10:27:35 +1200

    Created a dicerolling app and deployed it to GitHub pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.js File
+import React from 'react';
+import RollDice from './components/RollDice'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+function App() {
+	return (
+		<div>
+			<RollDice />
+		</div>
+	);
+}
+
+export default App;
